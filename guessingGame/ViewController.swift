8f23f034b57a1abdf4a06d43f9e0390d738ca185
kaylant <kaylant@gmail.com>
2016-04-26 19:03:52 -0500

    initial commit of game with random numbers and user input

--- guessingGame/ViewController.swift
+++ guessingGame/ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userGuessTextField: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func guessNum(sender: AnyObject) {
+        // let is used to define variables that never change
+        // UInt32 is a big integer
+        // Set type to string to be able to compare with user input
+        let fingersUp = String(arc4random_uniform(6))
+        
+        if userGuessTextField.text == "" {
+            resultLabel.text = "Please enter a number."
+        } else if fingersUp == userGuessTextField.text {
+            resultLabel.text = "You're right!"
+        } else {
+            resultLabel.text = "Wrong! It was " + fingersUp
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
